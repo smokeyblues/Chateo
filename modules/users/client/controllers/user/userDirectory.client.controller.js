68a55c46e5de8c4467986fa9a261f1f1ea196527
Aaron Pennington <aaron.l.pennington@gmail.com>
2016-12-08 15:28:04 -0700

    Web Speech API is working

--- modules/users/client/controllers/user/userDirectory.client.controller.js
+++ modules/users/client/controllers/user/userDirectory.client.controller.js
@@ -4,8 +4,15 @@ angular.module('users.user').controller('UserDirectoryController', ['$scope', '$
   function ($scope, $filter, User) {
     User.query(function (data) {
       $scope.users = data;
+      $scope.userNames = $scope.users.map(function(element) {
+        return element.firstName + ' ' + element.lastName
+      });
+      $scope.grammar = '#JSGF V1.0; grammar userNames; public <userName> = ' + $scope.userNames.join(' | ') + ' ;'
       $scope.buildPager();
+      console.log($scope.userNames);
     });
+
+    console.log("the controller is here.");
 
     $scope.buildPager = function () {
       $scope.pagedSearchItems = [];
@@ -27,5 +34,33 @@ angular.module('users.user').controller('UserDirectoryController', ['$scope', '$
     $scope.pageChanged = function () {
       $scope.figureOutItemsToDisplay();
     };
+
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+    var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+    $scope.SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    $scope.SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+    $scope.SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+    $scope.recognition = new SpeechRecognition();
+    $scope.speechRecognitionList = new SpeechGrammarList();
+    $scope.speechRecognitionList.addFromString($scope.grammar, 1);
+    $scope.recognition.grammars = $scope.speechRecognitionList;
+    $scope.recognition.lang = 'un-US';
+    $scope.recognition.interimResults = false;
+    $scope.recognition.maxAlternatives = 1;
+
+    $scope.webSpeech = function () {
+      // console.log($scope.grammar);
+      $scope.recognition.start();
+      console.log('Ready to receive voice command');
+    };
+
+    $scope.recognition.onresult = function(event) {
+      var last = event.results.length - 1;
+      var name = event.results[last][0].transcript;
+      $scope.vocalResponse = event.results;
+      console.log(name);
+    }
   }
 ]);
